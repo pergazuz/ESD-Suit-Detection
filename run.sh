371abc31c9198156df02eff94c1a33e9c758b69c
Panupong Keawkhao <panupongtongfah@gmail.com>
2023-07-22 12:39:21 +0700

    .sh

--- run.sh
+++ run.sh
@@ -3,12 +3,9 @@
 # Navigate to the frontend directory
 cd "Demo yolo"
 
-# Install npm dependencies and run the dev server in a new terminal
-npm install
-osascript -e 'tell app "Terminal" to do script "npm run dev"' 
+# Install npm dependencies and run the dev server in the background
 
-# Navigate back to the root directory
-cd ..
+npm run dev &
 
-# Start the backend server
-python YoloAPI.py
+# Start the backend server in a new terminal window
+osascript -e 'tell app "Terminal" to do script "python ../YoloAPI.py"'
